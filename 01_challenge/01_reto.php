aecb9ce9129e859a36ccc16cfcb16f11cb3db2e1
RoeZX <spsandoval94@gmail.com>
2023-11-05 06:33:46 -0300

    feature: convertir el string a minúsculas

--- 01_challenge/01_reto.php
+++ 01_challenge/01_reto.php
@@ -1,5 +1,5 @@
 <?php
 
-$string = trim(file_get_contents('https://codember.dev/data/message_01.txt'));
+$string = strtolower(trim(file_get_contents('https://codember.dev/data/message_01.txt')));
 $array_words = array_count_values(explode(' ',$string));
 foreach ($array_words as $word => $cant) echo $word.$cant;
